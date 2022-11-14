95fb6902a3cc7228258c4908851cb1de84276295
Duronne <duronne.tks@gmail.com>
2022-11-14 14:51:08 -0300

    projeto com erro knex

---
+++ src/server.js
@@ -0,0 +1,83 @@
+require('express-async-errors');
+const migrationsRun = require('./database/sqlite/migrations')
+const AppError = require('./utils/AppError')
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+migrationsRun()
+
+app.use(( error, request, response, next ) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    });
+  }
+
+    console.error(error);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+
+});
+
+const PORT = 3333;
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}.`)
+})
+
+// Mandatory values
+// app.get('/message/:id/:user', ( request, response ) => {
+//   const { id, user } = request.params;
+  
+//   response.send(`
+//   Id da mensagem: ${id}
+//   Para o usuário: ${user}
+//   `)
+// });
+
+//Optional values (Query Params)
+
+// app.get('/users', (request, response) => {
+//   const {page, limit} = request.query;
+
+//   response.send(`Página ${page}. Mostrar ${limit}`);
+// });
+
+// Function moved to folder /routes/users.routes.js
+// app.post('/users', (request, response) => {
+//   const { name, email, password } = request.body;
+
+//   response.send(`Usuário: ${name}. E-mail: ${email}. E a senha é ${password}.`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
